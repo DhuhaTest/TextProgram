8d3ab84eaa002485d90166983fdcef04bb1339b2
DhuhaTest <bahbooha92@yahoo.com>
2024-11-17 19:17:09 +0100

    Lines & Chars

--- src/TextInput.java
+++ src/TextInput.java
@@ -5,14 +5,27 @@ public class TextInput {
         Scanner scanner = new Scanner (System.in);
         System.out.println("Enter your text in different lines, type 'Stop' to exit: ");
 
+
+        int lines= 0;
+        int chars= 0;
+
         while (true) {
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
+            if (input.isEmpty()) {
+                continue;
+            }
             if (input.equalsIgnoreCase("stop")) {
-                System.out.println("Program stopped.");
+                System.out.println("Here is the result: ");
                 break;
             }
 
+            lines++;
+            chars += input.length();
         }
+
+        System.out.println("Number of lines (excluding 'stop'): " + lines);
+        System.out.println("Number of characters (excluding 'stop'): " + chars);
+
         scanner.close();
 
     }
