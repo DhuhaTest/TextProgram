c3f4d58ebadc2c24837af707a9f68980aece92bd
DhuhaTest <bahbooha92@yahoo.com>
2024-11-17 18:52:01 +0100

    Text and Stop

--- src/TextInput.java
+++ src/TextInput.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TextInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Enter your text in different lines, type 'Stop' to exit: ");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("stop")) {
+                System.out.println("Program stopped.");
+                break;
+            }
+
+        }
+        scanner.close();
+
+    }
 }
